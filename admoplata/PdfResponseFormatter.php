4bd0c738784ea0e69185b0dd3063410c7d80aa6c
Pavel <pavlinter@gmail.com>
2015-01-16 08:45:38 +0200

    Fixed \Closure

--- admoplata/PdfResponseFormatter.php
+++ admoplata/PdfResponseFormatter.php
@@ -36,36 +36,4 @@ class PdfResponseFormatter extends \robregonm\pdf\PdfResponseFormatter
 	 * Default to 'Portrait'
 	 */
 	public $orientation = 'Landscape';
-
-	/**
-	 * Formats response HTML in PDF
-	 *
-	 * @param Response $response
-	 */
-	protected function formatPdf($response)
-	{
-		$mpdf = new \mPDF($this->mode,
-			$this->format,
-			$this->defaultFontSize,
-			$this->defaultFont,
-			$this->marginLeft,
-			$this->marginRight,
-			$this->marginTop,
-			$this->marginBottom,
-			$this->marginHeader,
-			$this->marginFooter,
-			$this->orientation
-		);
-
-		foreach ($this->options as $key => $option) {
-			$mpdf->$key = $option;
-		}
-
-		if ($this->beforeRender instanceof \Closure) {
-			call_user_func($this->beforeRender, $mpdf, $response->data);
-		}
-
-		$mpdf->WriteHTML($response->data);
-		return $mpdf->Output('', 'S');
-	}
 }
